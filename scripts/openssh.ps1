d0f802cc8e0fd010b6a4d45b5fade70e7258c018
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-17 09:36:36 +0200

    Update to OpenSSH 7.3p1-2

--- scripts/openssh.ps1
+++ scripts/openssh.ps1
@@ -5,7 +5,7 @@ param (
 Write-Host "AutoStart: $AutoStart"
 
 # setup openssh
-$ssh_download_url = "http://www.mls-software.com/files/setupssh-6.7p1-1.exe"
+$ssh_download_url = "http://www.mls-software.com/files/setupssh-7.3p1-2.exe"
 
 if (!(Test-Path "C:\Program Files\OpenSSH\bin\ssh.exe")) {
     Write-Host "Downloading $ssh_download_url"
