7c08fc12149e7a0dd36a244c74b67c04443f45a3
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-17 21:39:38 +0200

    Use bat as provision scripts through SSH

--- scripts/enable-winrm.bat
+++ scripts/enable-winrm.bat
@@ -1,2 +1,6 @@
-rem Enable-NetFirewallRule for WinRM
-netsh advfirewall firewall add rule name="Port 5985" dir=in action=allow protocol=TCP localport=5985
+if exist a:\enable-winrm.ps1 (
+  powershell -File a:\enable-winrm.ps1
+) else (
+  rem Enable-NetFirewallRule for WinRM
+  netsh advfirewall firewall add rule name="Port 5985" dir=in action=allow protocol=TCP localport=5985  
+)
---
+++ scripts/set-powerplan.bat
@@ -0,0 +1,3 @@
+if exist a:\set-powerplan.ps1 (
+  powershell -file a:\set-powerplan.ps1
+)
