3b71fcce1589666bc96128c8946b64f021b476c8
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-18 07:47:18 +0200

    Always add firewall rule

--- scripts/enable-winrm.bat
+++ scripts/enable-winrm.bat
@@ -1,6 +1,6 @@
 if exist a:\enable-winrm.ps1 (
   powershell -File a:\enable-winrm.ps1
-) else (
-  rem Enable-NetFirewallRule for WinRM
-  netsh advfirewall firewall add rule name="Port 5985" dir=in action=allow protocol=TCP localport=5985  
 )
+
+rem Enable-NetFirewallRule for WinRM
+netsh advfirewall firewall add rule name="Port 5985" dir=in action=allow protocol=TCP localport=5985
