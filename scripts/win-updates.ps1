143eda3e4c09d9a501f593e6be2f88eb635aae83
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-18 11:09:09 +0200

    Skip hanging updates

--- scripts/win-updates.ps1
+++ scripts/win-updates.ps1
@@ -93,6 +93,10 @@ function Install-WindowsUpdates() {
             [bool]$addThisUpdate = $false
             if ($Update.InstallationBehavior.CanRequestUserInput) {
                 LogWrite "> Skipping: $($Update.Title) because it requires user input"
+            } else if ($Update.Title.Contains("KB3188966")) {
+                LogWrite "> Skipping: $($Update.Title) because it will hang forever"
+            } else if ($Update.Title.Contains("KB3189866")) {
+                LogWrite "> Skipping: $($Update.Title) because it will hang forever"
             } else {
                 if (!($Update.EulaAccepted)) {
                     LogWrite "> Note: $($Update.Title) has a license agreement that must be accepted. Accepting the license."
