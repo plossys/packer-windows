fca8c698347ff0eecd53b29750645fc141c943c4
Stefan Scherer <scherer_stefan@icloud.com>
2016-10-08 14:33:04 +0200

    Use shell provisioner

---
+++ scripts/docker/install-docker.bat
@@ -0,0 +1,4 @@
+powershell -File A:\scripts\docker\add-docker-group.ps1
+powershell -File A:\scripts\docker\install-docker.ps1
+powershell -File A:\scripts\docker\open-docker-insecure-port.ps1
+powershell -File A:\scripts\docker\remove-docker-key-json.ps1
