e58935caa6cdba058d591014324edbe5806f8708
Stefan Scherer <scherer_stefan@icloud.com>
2016-10-11 14:48:34 +0200

    Add generale port 5985 firewall rule

--- scripts/enable-winrm.bat
+++ scripts/enable-winrm.bat
@@ -1,6 +1,3 @@
 if exist a:\enable-winrm.ps1 (
   powershell -File a:\enable-winrm.ps1
 )
-
-rem Enable-NetFirewallRule for WinRM
-netsh advfirewall firewall add rule name="Port 5985" dir=in action=allow protocol=TCP localport=5985
--- scripts/enable-winrm.ps1
+++ scripts/enable-winrm.ps1
@@ -9,5 +9,6 @@ winrm set winrm/config/client/auth '@{Basic="true"}'
 winrm set winrm/config/listener?Address=*+Transport=HTTP '@{Port="5985"}'
 netsh advfirewall firewall set rule group="Windows Remote Administration" new enable=yes
 netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new enable=yes action=allow
+netsh advfirewall firewall add rule name="Port 5985" dir=in action=allow protocol=TCP localport=5985
 Set-Service winrm -startuptype "auto"
 Restart-Service winrm
