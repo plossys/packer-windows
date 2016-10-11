5510645d16e4b27a052cf8104bf8a3a622badb96
Stefan Scherer <scherer_stefan@icloud.com>
2016-10-11 18:09:07 +0200

    Fix shutdown

--- scripts/docker/shutdown-and-turn-on-firewall.ps1
+++ scripts/docker/shutdown-and-turn-on-firewall.ps1
@@ -1,6 +1,6 @@
-Write-Host "Turning on Firewall in 10 seconds ..."
-Start-Sleep 10
+Write-Host 'Turning on Firewall in 2 seconds ...'
+Start-Sleep 2
 netsh advfirewall set allprofiles state on
 
-Write-Host "Shutting down system ..."
-shutdown /s /t 10 /f /d p:4:1 /c \"Packer Shutdown\"
+Write-Host 'Shutting down system ...'
+shutdown /s /t 10 /f /d p:4:1 /c 'Packer Shutdown'
